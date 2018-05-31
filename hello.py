65e9b5b741951d225ecbdf34f7a5874147ac8126
Vikas-Kshirsagar <vikas.kshirsagar@outlook.com>
2018-05-31 12:08:21 -0400

    add all files with git ignore

--- hello.py
+++ hello.py
@@ -1,16 +1,23 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
-@app.route('/admin')
-def hello_admin():
-    return 'Hello Admin'
-@app.route('/guest/<guest>')
-def hello_guest(guest):
- return 'Hello %s as Guest' % guest
-@app.route('/user/<name>')
-def hello_user(name):
- if name=='admin':
-     return redirect(url_for('hello_admin'))
- else:
-     return redirect(url_for('hello_guest',guest=name))
+@app.route('/')
+def main():
+    return render_template('index.html')
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+@app.route('/signUp',methods=['POST'])
+def signUp():
+ 
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+ 
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 if __name__ == '__main__':
- app.run(debug=True)
+    app.run(debug=True)
